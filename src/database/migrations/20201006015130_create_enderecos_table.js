b5a010f064a3b696efa9d6557b8758f96ae6d5be
jonas97salatiel <jonas97salatiel@gmail.com>
2020-11-24 23:26:35 -0300

    Ajustando construção da tabela enderecos

--- src/database/migrations/20201006015130_create_enderecos_table.js
+++ src/database/migrations/20201006015130_create_enderecos_table.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('enderecos', function(table){
 
         table.increments('idEndereco').primary();
-        table.string('cep', 7);
+        table.string('cep', 8);
         table.string('rua', 50);
         table.string('numero', 10);
         table.string('complemento', 50);
