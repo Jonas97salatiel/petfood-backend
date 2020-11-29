15a8f8787cb365862ecbbbc09369600b974ab458
jonas97salatiel <jonas97salatiel@gmail.com>
2020-11-29 15:41:19 -0300

    Incluindo endereço na consulta de usuarios

--- src/controllers/ClienteControllers.js
+++ src/controllers/ClienteControllers.js
@@ -22,7 +22,7 @@ module.exports = {
         try {
             const results = await knex('clientes')
             .join('enderecos', 'clientes.userId', '=', 'enderecos.userId')
-            .join('usuarios', 'clientes.id', '=', 'usuarios.id')
+            .join('usuarios', 'clientes.id', '=', 'id')
             .where('clientes.userId', userId)
             return res.json(results);
         } catch (error) {
