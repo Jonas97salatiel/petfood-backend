16365f38a524156ed9556fa7325755c66dee1ceb
jonas97salatiel <jonas97salatiel@gmail.com>
2020-11-29 12:00:25 -0300

    Adicionando API de pagamentos

--- src/controllers/PedidosControllers.js
+++ src/controllers/PedidosControllers.js
@@ -78,7 +78,76 @@ module.exports = {
                 .then(client => client.security.encrypt(card))
                 .then(card_hash => console.log(card_hash))
 
+            const params = {
+                    "amount": "21000",
+                    "card_number": "4111111111111111",
+                    "card_cvv": "123",
+                    "card_expiration_date": "1225",
+                    "card_holder_name": "abc",
+                    "customer": {
+                      "external_id": "#3311",
+                      "name": "abc",
+                      "type": "individual",
+                      "country": "br",
+                      "email": "mopheus@nabucodonozor.com",
+                      "documents": [
+                        {
+                          "type": "cpf",
+                          "number": "00000000000"
+                        }
+                      ],
+                      "phone_numbers": ["+5511999998888", "+5511888889999"],
+                      "birthday": "1965-01-01"
+                    },
+                    "billing": {
+                      "name": "Trinity Moss",
+                      "address": {
+                        "country": "br",
+                        "state": "sp",
+                        "city": "Cotia",
+                        "neighborhood": "Rio Cotia",
+                        "street": "Rua Matrix",
+                        "street_number": "9999",
+                        "zipcode": "06714360"
+                      }
+                    },
+                    "shipping": {
+                      "name": "Neo Reeves",
+                      "fee": "1000",
+                      "delivery_date": "2000-12-21",
+                      "expedited": True,
+                      "address": {
+                        "country": "br",
+                        "state": "sp",
+                        "city": "Cotia",
+                        "neighborhood": "Rio Cotia",
+                        "street": "Rua Matrix",
+                        "street_number": "9999",
+                        "zipcode": "06714360"
+                      }
+                    },
+                    "items": [
+                      {
+                        "id": "r123",
+                        "title": "Red pill",
+                        "unit_price": "10000",
+                        "quantity": "1",
+                        "tangible": True
+                      },
+                      {
+                        "id": "b123",
+                        "title": "Blue pill",
+                        "unit_price": "10000",
+                        "quantity": "1",
+                        "tangible": True
+                      }
+                    ]
+                }
 
+            const pagarmeTransaction = await pagarme.client.transactions.create(params);
+
+            console.log(pagarmeTransaction);
+            
             return res.status(200).json({ success: 'Pedido cadastrado com sucesso!' })
 
         } catch (error) {
