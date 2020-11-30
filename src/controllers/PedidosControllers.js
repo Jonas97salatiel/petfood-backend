307b8d39b88269f2d02e0a1816840df9ef6be2d5
jonas97salatiel <jonas97salatiel@gmail.com>
2020-11-30 04:14:18 -0300

    Ajustando post da rota pedidos

--- src/controllers/PedidosControllers.js
+++ src/controllers/PedidosControllers.js
@@ -64,17 +64,17 @@ module.exports = {
 
         var items = [];
 
-            for (let index = 0; index < ListaProdutos.length; ) {
-            
-                var id = ListaProdutos[index].idProduto;
-                var title = ListaProdutos[index].nome;
-                var unit_price = ListaProdutos[index].valor * 100;
-                var quantity = ListaProdutos[index].qtd;
-                var tangible = true;                                   
-                
-                items.push({id, title, unit_price, quantity, tangible})
-                index++
-            }
+        for (let index = 0; index < ListaProdutos.length;) {
+
+            var id = ListaProdutos[index].idProduto;
+            var title = ListaProdutos[index].nome;
+            var unit_price = ListaProdutos[index].valor * 100;
+            var quantity = ListaProdutos[index].qtd;
+            var tangible = true;
+
+            items.push({ id, title, unit_price, quantity, tangible })
+            index++
+        }
 
 
         let listaProdutos = req.body.ListaProdutos;
@@ -83,142 +83,160 @@ module.exports = {
 
         console.log(items);
 
-         try {
+        try {
 
-        const datePagamentos = {
-            "amount": valorPedidoCents,
-            "card_number": varCartao,
-            "card_cvv": cvv,
-            "card_expiration_date": validadeExprt,
-            "card_holder_name": nometitular,
-            "customer": {
-                "external_id": "#3311",
-                "name": nometitular,
-                "type": "individual",
-                "country": "br",
-                "email": email,
-                "documents": [
-                    {
-                        "type": "cpf",
-                        "number": "30621143049"
+            const datePagamentos = {
+                "amount": valorPedidoCents,
+                "card_number": varCartao,
+                "card_cvv": cvv,
+                "card_expiration_date": validadeExprt,
+                "card_holder_name": nometitular,
+                "customer": {
+                    "external_id": "#3311",
+                    "name": nometitular,
+                    "type": "individual",
+                    "country": "br",
+                    "email": email,
+                    "documents": [
+                        {
+                            "type": "cpf",
+                            "number": cpf
+                        }
+                    ],
+                    "phone_numbers": [telefone],
+                    "birthday": "2000-01-01"
+                },
+                "billing": {
+                    "name": nomeUsuario,
+                    "address": {
+                        "country": "br",
+                        "state": uf,
+                        "city": cidade,
+                        "neighborhood": bairro,
+                        "street": rua,
+                        "street_number": numeroRua,
+                        "zipcode": cep
                     }
-                ],
-                "phone_numbers": [telefone],
-                "birthday": "2000-01-01"
-            },
-            "billing": {
-                "name": nomeUsuario,
-                "address": {
-                    "country": "br",
-                    "state": uf,
-                    "city": cidade,
-                    "neighborhood": bairro,
-                    "street": rua,
-                    "street_number": numeroRua,
-                    "zipcode": cep
-                }
-            },
-            "shipping": {
-                "name": nomeParceiro,
-                "fee": 0010,
-                "delivery_date": currentDate,
-                "expedited": true,
-                "address": {
-                    "country": "br",
-                    "state": ufParceiro,
-                    "city": cidadeParceiro,
-                    "neighborhood": bairroParceiro,
-                    "street": ruaParceiro,
-                    "street_number": numeroRuaParceiro,
-                    "zipcode": cepParceiro
-                }
-            },
-            "items": items
-        }
+                },
+                "shipping": {
+                    "name": nomeParceiro,
+                    "fee": 0010,
+                    "delivery_date": currentDate,
+                    "expedited": true,
+                    "address": {
+                        "country": "br",
+                        "state": ufParceiro,
+                        "city": cidadeParceiro,
+                        "neighborhood": bairroParceiro,
+                        "street": ruaParceiro,
+                        "street_number": numeroRuaParceiro,
+                        "zipcode": cepParceiro
+                    }
+                },
+                "items": items
+            }
 
-        console.log(datePagamentos);
+            console.log(datePagamentos);
 
-        //    await pagarme.client.connect({ api_key: 'ak_test_82qgXOppwHF79yNxfhXHTIty2rMqcE' })
-        //         .then(client => client.transactions.create({
-        //             "amount": valorPedidoCents,
-        //             "card_number": varCartao,
-        //             "card_cvv": cvv,
-        //             "card_expiration_date": validadeExprt,
-        //             "card_holder_name": nometitular,
-        //             "customer": {
-        //                 "external_id": "#3311",
-        //                 "name": nometitular,
-        //                 "type": "individual",
-        //                 "country": "br",
-        //                 "email": email,
-        //                 "documents": [
-        //                     {
-        //                         "type": "cpf",
-        //                         "number": cpf
-        //                     }
-        //                 ],
-        //                 "phone_numbers": [telefone],
-        //                 "birthday": "2000-01-01"
-        //             },
-        //             "billing": {
-        //                 "name": nomeUsuario,
-        //                 "address": {
-        //                     "country": "br",
-        //                     "state": uf,
-        //                     "city": cidade,
-        //                     "neighborhood": bairro,
-        //                     "street": rua,
-        //                     "street_number": numeroRua,
-        //                     "zipcode": cep
-        //                 }
-        //             },
-        //             "shipping": {
-        //                 "name": nomeParceiro,
-        //                 "fee": 0010,
-        //                 "delivery_date": currentDate,
-        //                 "expedited": true,
-        //                 "address": {
-        //                     "country": "br",
-        //                     "state": ufParceiro,
-        //                     "city": cidadeParceiro,
-        //                     "neighborhood": bairroParceiro,
-        //                     "street": ruaParceiro,
-        //                     "street_number": numeroRuaParceiro,
-        //                     "zipcode": cepParceiro
-        //                 }
-        //             },
-        //             "items": items
-        //         }))
-        //         .then(transaction => console.log(transaction))
+            await pagarme.client.connect({ api_key: 'ak_test_82qgXOppwHF79yNxfhXHTIty2rMqcE' })
+                .then(client => client.transactions.create({
+                    "amount": 21000,
+                    "card_number": "4111111111111111",
+                    "card_cvv": "123",
+                    "card_expiration_date": "0922",
+                    "card_holder_name": "Morpheus Fishburne",
+                    "customer": {
+                        "external_id": "#3311",
+                        "name": "Morpheus Fishburne",
+                        "type": "individual",
+                        "country": "br",
+                        "email": "mopheus@nabucodonozor.com",
+                        "documents": [
+                            {
+                                "type": "cpf",
+                                "number": "30621143049"
+                            }
+                        ],
+                        "phone_numbers": ["+5511999998888", "+5511888889999"],
+                        "birthday": "1965-01-01"
+                    },
+                    "billing": {
+                        "name": "Trinity Moss",
+                        "address": {
+                            "country": "br",
+                            "state": "sp",
+                            "city": "Cotia",
+                            "neighborhood": "Rio Cotia",
+                            "street": "Rua Matrix",
+                            "street_number": "9999",
+                            "zipcode": "06714360"
+                        }
+                    },
+                    "shipping": {
+                        "name": "Neo Reeves",
+                        "fee": 1000,
+                        "delivery_date": "2000-12-21",
+                        "expedited": true,
+                        "address": {
+                            "country": "br",
+                            "state": "sp",
+                            "city": "Cotia",
+                            "neighborhood": "Rio Cotia",
+                            "street": "Rua Matrix",
+                            "street_number": "9999",
+                            "zipcode": "06714360"
+                        }
+                    },
+                    "items": [
+                        {
+                            "id": "r123",
+                            "title": "Red pill",
+                            "unit_price": 10000,
+                            "quantity": 1,
+                            "tangible": true
+                        },
+                        {
+                            "id": "b123",
+                            "title": "Blue pill",
+                            "unit_price": 10000,
+                            "quantity": 1,
+                            "tangible": true
+                        }
+                    ]
+                }))
+                .then(transaction => console.log(transaction))
+                .catch(function(error){
+                    console.log(error);
+                })
 
- 
-                // await knex('pedidos').insert({
 
-                //     valorPedido: valorPedido,
-                //     status: status,
-                //     numeroTransacao: numeroTransacao,
-                //     idCliente: idCliente,
-                //     idFormaPagamento: idFormaPagamento,
-                //     idCuponsDesconto: idCuponsDesconto,
-                //     idParceiro: idParceiro
-    
-                // });
-    
-                // const idPedidos = await knex('pedidos').where({ numeroTransacao: numeroTransacao }).select('idPedidos');
-                // console.log(idPedidos);
-                
-                // for (let index = 0; index < listaProdutos.length;) {
-    
-                //     const { idProduto, qtd } = listaProdutos[index];
-    
-                //     await knex('pedidos_produtos').insert({
-                //         quantidade: qtd,
-                //         idPedidos: idPedidos[0].idPedidos,
-                //         idProduto: idProduto
-                //     })
-    
-                //     index++
-                // }
+            // await knex('pedidos').insert({
+
+            //     valorPedido: valorPedido,
+            //     status: status,
+            //     numeroTransacao: numeroTransacao,
+            //     idCliente: idCliente,
+            //     idFormaPagamento: idFormaPagamento,
+            //     idCuponsDesconto: idCuponsDesconto,
+            //     idParceiro: idParceiro
+
+            // });
+
+            // const idPedidos = await knex('pedidos').where({ numeroTransacao: numeroTransacao }).select('idPedidos');
+            // console.log(idPedidos);
+
+            // for (let index = 0; index < listaProdutos.length;) {
+
+            //     const { idProduto, qtd } = listaProdutos[index];
+
+            //     await knex('pedidos_produtos').insert({
+            //         quantidade: qtd,
+            //         idPedidos: idPedidos[0].idPedidos,
+            //         idProduto: idProduto
+            //     })
+
+            //     index++
+            // }
 
             return res.status(200).json({ success: 'Pedido cadastrado com sucesso!' })
 
