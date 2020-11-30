3367c5b565b007409790ea202f9f9f744a259309
jonas97salatiel <jonas97salatiel@gmail.com>
2020-11-30 01:45:16 -0300

    Ajustando post da rota pedidos

--- src/controllers/PedidosControllers.js
+++ src/controllers/PedidosControllers.js
@@ -63,17 +63,23 @@ module.exports = {
 
         var items = [];
 
-        for (let index = 0; index < ListaProdutos.length; ) {
+        await function trasnformaArray(){
+
+            for (let index = 0; index < ListaProdutos.length; ) {
             
-            var id = ListaProdutos[index].idProduto;
-            var title = ListaProdutos[index].nome;
-            var unit_price = ListaProdutos[index].valor * 100;
-            var quantity = ListaProdutos[index].qtd;
-            var tangible = true;                                   
-            
-            items.push({id, title, unit_price, quantity, tangible})
-            index++
+                var id = ListaProdutos[index].idProduto;
+                var title = ListaProdutos[index].nome;
+                var unit_price = ListaProdutos[index].valor * 100;
+                var quantity = ListaProdutos[index].qtd;
+                var tangible = true;                                   
+                
+                items.push({id, title, unit_price, quantity, tangible})
+                index++
+            }
+
         }
+
+       
 
 
 
@@ -85,101 +91,89 @@ module.exports = {
 
          try {
 
-            // await knex('pedidos').insert({
 
-            //     valorPedido: valorPedido,
-            //     status: status,
-            //     numeroTransacao: numeroTransacao,
-            //     idCliente: idCliente,
-            //     idFormaPagamento: idFormaPagamento,
-            //     idCuponsDesconto: idCuponsDesconto,
-            //     idParceiro: idParceiro
 
-            // });
+           await pagarme.client.connect({ api_key: 'ak_test_82qgXOppwHF79yNxfhXHTIty2rMqcE' })
+                .then(client => client.transactions.create({
+                    "amount": 21000,
+                    "card_number": valorPedidoCents,
+                    "card_cvv": varCartao,
+                    "card_expiration_date": validadeExprt,
+                    "card_holder_name": nometitular,
+                    "customer": {
+                        "external_id": "#3311",
+                        "name": nometitular,
+                        "type": "individual",
+                        "country": "br",
+                        "email": email,
+                        "documents": [
+                            {
+                                "type": "cpf",
+                                "number": cpf
+                            }
+                        ],
+                        "phone_numbers": [telefone],
+                        "birthday": "2000-01-01"
+                    },
+                    "billing": {
+                        "name": nomeUsuario,
+                        "address": {
+                            "country": "br",
+                            "state": uf,
+                            "city": cidade,
+                            "neighborhood": bairro,
+                            "street": rua,
+                            "street_number": numeroRua,
+                            "zipcode": cep
+                        }
+                    },
+                    "shipping": {
+                        "name": nomeParceiro,
+                        "fee": 0000,
+                        "delivery_date": currentDate,
+                        "expedited": true,
+                        "address": {
+                            "country": "br",
+                            "state": ufParceiro,
+                            "city": cidadeParceiro,
+                            "neighborhood": bairroParceiro,
+                            "street": ruaParceiro,
+                            "street_number": numeroRuaParceiro,
+                            "zipcode": cepParceiro
+                        }
+                    },
+                    "items": items
+                }))
+                .then(transaction => console.log(transaction))
 
-            // const idPedidos = await knex('pedidos').where({ numeroTransacao: numeroTransacao }).select('idPedidos');
-            // console.log(idPedidos);
-            
-            // for (let index = 0; index < listaProdutos.length;) {
 
-            //     const { idProduto, qtd } = listaProdutos[index];
+                await knex('pedidos').insert({
 
-            //     await knex('pedidos_produtos').insert({
-            //         quantidade: qtd,
-            //         idPedidos: idPedidos[0].idPedidos,
-            //         idProduto: idProduto
-            //     })
-
-            //     index++
-            // }
-
-            // pagarme.client.connect({ api_key: 'ak_test_82qgXOppwHF79yNxfhXHTIty2rMqcE' })
-            //     .then(client => client.transactions.create({
-            //         "amount": 21000,
-            //         "card_number": valorPedidoCents,
-            //         "card_cvv": varCartao,
-            //         "card_expiration_date": validadeExprt,
-            //         "card_holder_name": nometitular,
-            //         "customer": {
-            //             "external_id": "#3311",
-            //             "name": nometitular,
-            //             "type": "individual",
-            //             "country": "br",
-            //             "email": email,
-            //             "documents": [
-            //                 {
-            //                     "type": "cpf",
-            //                     "number": cpf
-            //                 }
-            //             ],
-            //             "phone_numbers": [telefone],
-            //             "birthday": "2000-01-01"
-            //         },
-            //         "billing": {
-            //             "name": nomeUsuario,
-            //             "address": {
-            //                 "country": "br",
-            //                 "state": uf,
-            //                 "city": cidade,
-            //                 "neighborhood": bairro,
-            //                 "street": rua,
-            //                 "street_number": numeroRua,
-            //                 "zipcode": cep
-            //             }
-            //         },
-            //         "shipping": {
-            //             "name": nomeParceiro,
-            //             "fee": 0000,
-            //             "delivery_date": currentDate,
-            //             "expedited": true,
-            //             "address": {
-            //                 "country": "br",
-            //                 "state": ufParceiro,
-            //                 "city": cidadeParceiro,
-            //                 "neighborhood": bairroParceiro,
-            //                 "street": ruaParceiro,
-            //                 "street_number": numeroRuaParceiro,
-            //                 "zipcode": cepParceiro
-            //             }
-            //         },
-            //         "items": [
-            //             {
-            //                 "id": "r123",
-            //                 "title": "Red pill",
-            //                 "unit_price": 10000,
-            //                 "quantity": 1,
-            //                 "tangible": true
-            //             },
-            //             {
-            //                 "id": "b123",
-            //                 "title": "Blue pill",
-            //                 "unit_price": 10000,
-            //                 "quantity": 1,
-            //                 "tangible": true
-            //             }
-            //         ]
-            //     }))
-            //     .then(transaction => console.log(transaction))
+                    valorPedido: valorPedido,
+                    status: status,
+                    numeroTransacao: numeroTransacao,
+                    idCliente: idCliente,
+                    idFormaPagamento: idFormaPagamento,
+                    idCuponsDesconto: idCuponsDesconto,
+                    idParceiro: idParceiro
+    
+                });
+    
+                const idPedidos = await knex('pedidos').where({ numeroTransacao: numeroTransacao }).select('idPedidos');
+                console.log(idPedidos);
+                
+                for (let index = 0; index < listaProdutos.length;) {
+    
+                    const { idProduto, qtd } = listaProdutos[index];
+    
+                    await knex('pedidos_produtos').insert({
+                        quantidade: qtd,
+                        idPedidos: idPedidos[0].idPedidos,
+                        idProduto: idProduto
+                    })
+    
+                    index++
+                }
 
             return res.status(200).json({ success: 'Pedido cadastrado com sucesso!' })
 
